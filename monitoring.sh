456c567c8a9bbbb8b5790007847a31da2624f934
ysantos- <83174271+YuriASN@users.noreply.github.com>
2026-02-25 00:05:53 +0000

    Fixed the script
    LVM command didn't exist, and a few others relied on the position, now ir relies on the words.

--- monitoring.sh
+++ monitoring.sh
@@ -4,7 +4,7 @@ ARCH=$(uname -a)
 
 PPROCSS=$(lscpu | grep "Socket(s)" | awk '{print $2}')
 
-VPROCSS=$(lscpu | sed -n '5p' | awk '{print $2}')
+VPROCSS=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
 
 RAMUSE=$(free -h | sed -n '2p' | awk '{print $3 "/" $2}' | tr "i" "b")
 
@@ -12,28 +12,28 @@ RAMPRC=$(free | sed -n '2p' | awk '{printf("%.2f"), $3/$2*100}')
 
 DISK=$(df -h --total | grep total | awk '{print $3 "/" $2 " (" $5 ")"}')
 
-CPU=$(uptime | awk '{print $9}' | sed 's/,//')
+CPU=$(top -bn 1 | sed -n '3p' | cut -d "," -f 4 | awk '{printf("%.1f\n"), 100-$1}')
 
 LSTRBT=$(uptime -s | cut -d ":" -f 1,2)
 
-LVM=$(lvdisplay | grep -c "available")
+LVM=$(cat /proc/partitions | grep -c "dm-")
 
 LVMDSP=$(if [ $LVM -eq "0" ]; then echo no; else echo yes; fi)
 
 CONNC=$(ss -t | grep -c "ESTAB")
 
-USERS=$(w | sed -n '1p' | cut -d " " -f 7)
+USERS=$(users | wc -w)
 
 IPADDRS=$(hostname -I)
 
 MACADDRS=$(ip -o link | cut -d " " -f 20 | sed -n '2p')
 
-SUDO=$(journalctl _COMM=sudo | GREP -c "COMMAND")
+SUDO=$(journalctl -q _COMM=sudo | grep -c "COMMAND")
 
 wall "	#Architecture: $ARCH
 	#CPU physical: $PPROCSS
 	#vCPU: $VPROCSS
-	#Memory Usage: $RAMUSE ($RAMPEC%)
+	#Memory Usage: $RAMUSE ($RAMPRC%)
 	#Disk Usage: $DISK
 	#CPU load: $CPU%
 	#Last boot: $LSTRBT
@@ -41,4 +41,5 @@ wall "	#Architecture: $ARCH
 	#Connections TCP: $CONNC ESTABLISHED
 	#User log: $USERS
 	#Network: IP $IPADDRS ($MACADDRS)
-	#Sudo: $SUDO cmd"
+	#Sudo: $SUDO cmd
+"
